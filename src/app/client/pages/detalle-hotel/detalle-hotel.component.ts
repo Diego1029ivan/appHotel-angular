20b588b959cb0f934a0f8e75dfa2b79cc6b41113
Diego1029ivan <dialvaradov@alumno.unsm.edu.pe>
2023-03-10 14:53:49 -0500

    arreglo css

--- src/app/client/pages/detalle-hotel/detalle-hotel.component.ts
+++ src/app/client/pages/detalle-hotel/detalle-hotel.component.ts
@@ -5,6 +5,10 @@ import { HotelesService } from '../../services/hoteles.service';
 import { Hoteles } from '../../../interfaces/hoteles';
 import { environment } from 'src/environments/environment';
 import Swal from 'sweetalert2';
+import { Bares } from '../../../interfaces/bares';
+import { Piscinas } from '../../../interfaces/piscinas';
+import { Cocheras } from '../../../interfaces/cocheras';
+import { Precioxtipohabitacion } from '../../../interfaces/precioxtipohabitacion';
 
 @Component({
   selector: 'app-detalle-hotel',
@@ -24,16 +28,11 @@ export class DetalleHotelComponent implements OnInit{
               private router:Router){}
   
   
-
+  Arraybares:Bares[]=[]
+  Arraypiscina:Piscinas[]=[]
+  Arraycochera:Cocheras[]=[]
+  Arrayprecio:Precioxtipohabitacion[]=[]
   ngOnInit() :void {
-
-    // Swal.fire({
-    //   position: 'top-end',
-    //   icon: 'success',
-    //   title: 'Your work has been saved',
-    //   showConfirmButton: false,
-    //   timer: 1500
-    // })
     
     this.activatedRoute.params
     .pipe(
@@ -42,12 +41,20 @@ export class DetalleHotelComponent implements OnInit{
         (hotel)=>{
         this.hotelid = hotel
         console.log(this.hotelid)
+        //console.log(this.hotelid.cantidadHabitacion, 'cantidad')
+
+        this.Arraybares = this.hotelid.bares
+        this.Arraypiscina = this.hotelid.piscinas
+        this.Arraycochera = this.hotelid.cocheras
+        this.Arrayprecio = this.hotelid.precioxtipohabitacion
         
-     
       }
       )
       
     }
+   
+  
+  
     
   
 }
