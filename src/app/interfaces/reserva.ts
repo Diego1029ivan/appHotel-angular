f342d2721f8a1aefa2f15ef12d5f17f2d02b5e49
EsauProgramando <eaveintemillap@alumno.unsm.edu.pe>
2023-03-13 22:50:32 -0500

    reserva terminado

--- src/app/interfaces/reserva.ts
+++ src/app/interfaces/reserva.ts
@@ -1,19 +1,18 @@
-import { Hoteles } from "./hoteles";
-import { Tipohab } from "./Tipohab";
-import { TipoPago } from "./TipoPago";
-import { Usuario } from "./usuario";
+import { Hoteles } from './hoteles';
+import { Tipohab } from './Tipohab';
+import { TipoPago } from './TipoPago';
+import { Usuario } from './usuario';
 
 export class Reserva {
-    
-    fechaEntrada:     Date;
-    fechaSalida:      Date;
-    adelantoReservas: number;
-    estado:           1;
-    cantidadHab:      number;
-    cantidad_dias:      number;
-    costo_total:      number;
-    hotel?:            Hoteles;
-    usuario?:          Usuario;
-    tipoPago:         TipoPago;
-    tipohab:          Tipohab;
+  fechaEntrada: Date;
+  fechaSalida: Date;
+  adelantoReservas: number;
+  estado: number = 1;
+  cantidadHab: number;
+  cantidad_dias: number;
+  costo_total: number;
+  hotel?: Hoteles;
+  usuario?: Usuario;
+  tipoPago: TipoPago;
+  tipohab: Tipohab;
 }
